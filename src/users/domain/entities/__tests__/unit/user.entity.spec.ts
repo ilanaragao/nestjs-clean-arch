679991b7033b14746c60c43c5a5aded4a26a5922
Ilan Aragão <ilan_aragao@hotmail.com>
2024-06-21 23:16:56 -0300

    test: add user entity getters methods unity tests

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -21,4 +21,29 @@ describe('UserEntity unit tests', () => {
     expect(sut.props.password).toBe(props.password);
     expect(sut.props.createdAt).toBeInstanceOf(Date);
   });
+
+  it('Get name method', () => {
+    expect(sut.name).toBeDefined();
+    expect(typeof sut.name).toBe('string');
+    expect(sut.name).toBe(props.name);
+  });
+
+  it('Get email method', () => {
+    expect(sut.email).toBeDefined();
+    expect(typeof sut.email).toBe('string');
+    expect(sut.email).toBe(props.email);
+  });
+
+  it('Get password method', () => {
+    expect(sut.password).toBeDefined();
+    expect(typeof sut.password).toBe('string');
+    expect(sut.password).toBe(props.password);
+  });
+
+  it('Get createdAt method', () => {
+    expect(sut.createdAt).toBeDefined();
+    expect(typeof sut.createdAt).toBe('object');
+    expect(sut.createdAt).toBeInstanceOf(Date);
+    expect(sut.createdAt).toBeInstanceOf(Date);
+  });
 });
